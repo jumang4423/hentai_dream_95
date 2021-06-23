146f2ad6e657e4d6dac232dde0236d6f254c0914
ryoma okuda <catontheskate4423@gmail.com>
2021-06-23 23:53:48 +0900

    completed v1

---
+++ README.md
@@ -0,0 +1,13 @@
+# hentai dream 95
+
+secret folder generator for really old way to hide hentais in your computer
+
+⚠︎CAUTION⚠︎ this method is already broken by one of friends, so prefer not to use it.
+
+![](_img/sad.png)
+
+# install
+
+```bash
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,65 @@
-use std::io::*;
 use std::fs;
+use std::io::*;
+use std::str::from_utf8;
 
 fn main() {
-    
-        // PASSWORD
-        print!(
-            "PASSWORD:"
-        );
-        stdout().flush().unwrap();
-        let mut password = String::new();
-        stdin().read_line(&mut password).expect("input error => ?");
+    // password
+    let password = scan_me("PASSWORD (LENGTH<=5):");
+    // file
+    let hentai = scan_me("HENTAI FILE:");
+    // folder name
+    let dir = scan_me("HENTAI DIRECTORY NAME:");
 
-        // DIRECTORY
-        print!(
-            "HENTAI FILE:"
-        );
-        stdout().flush().unwrap();
-        let mut hentai = String::new();
-        stdin().read_line(&mut hentai).expect("input error => ?");
+    println!("HENTAI DREAM...\n");
 
-        // FOLDER NAME
-        print!(
-            "HENTAI DIRECTORY NAME:"
-        );
-        stdout().flush().unwrap();
-        let mut dir = String::new();
-        stdin().read_line(&mut dir).expect("input error => ?");
+    // make directories (lol)
+    fs::create_dir_all(&dir).unwrap();
+    dft_folder_maker([].to_vec(), password.chars().count(), &dir);
 
-        // LETSGO
-
-        print!(
-            "HENTAI DREAM..."
-        );
-
-        hentai_dream(password, hentai, dir);
+    // put hentai file
+    println!("DREAM COMPLETED AT:");
+    println!("{}", put_hentai(&hentai, &dir, &password));
 }
 
-fn hentai_dream(password: String, hentai: String,dir: String) {
-    fs::create_dir_all(dir).unwrap();
+fn scan_me(print_data: &str) -> String {
+    print!("{}", print_data);
+    stdout().flush().unwrap();
+    let mut _scaned_string = String::new();
+    stdin()
+        .read_line(&mut _scaned_string)
+        .expect("input error => ?");
+
+    _scaned_string = String::from(from_utf8(&_scaned_string.replace("\n", "").into_bytes()).unwrap());
+    return _scaned_string;
+}
+
+fn dft_folder_maker(array: Vec<i32>, count: usize, dir: &String) {
+    if count != 0 {
+        for i in 0..10 {
+            let mut new_array = array.clone();
+            new_array.push(i);
+            let joined: String = String::from(dir.clone() + &String::from("/"))
+                + &new_array
+                    .clone()
+                    .iter()
+                    .map(|_temp| _temp.to_string())
+                    .collect::<Vec<String>>()
+                    .join("/");
+
+            fs::create_dir_all(joined).unwrap();
+            dft_folder_maker(new_array, count - 1, &dir);
+        }
+    }
+}
+
+fn put_hentai(hentai_file: &String, dir: &String, password: &String) -> String {
+
+    let joined: String = format!("{}/{}/{}", dir, &password
+    .chars()
+    .map(|_temp| _temp.to_string())
+    .collect::<Vec<String>>()
+    .join("/"),hentai_file);
+    fs::copy(hentai_file, &joined).unwrap();
+
+    joined
 }
